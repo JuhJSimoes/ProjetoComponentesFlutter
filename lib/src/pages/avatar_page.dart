1a241060bc438c944fd77ae4035bdeb6f1d0295f
JuhJSimoes <juianajsimoes@gmail.com>
2020-03-30 18:25:08 -0300

    Criando avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,12 +6,33 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://images.kerrangcdn.com/CristinaScabbia-Promo-2019.jpg?auto=compress&fit=crop&w=1200'),
+              radius: 23.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JJS'),
+              backgroundColor: Colors.blue[200],
+              
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          }),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://fsa.zobj.net/crop.php?r=TsoEnWW2nPZsSBayVhuuVnr13AwO0d1iSyhiFYA8xWk6Jsae285x55ShOKbmOLaWtgL9P4TTE4AWv7YMN7-kH9EmGDlwjM5tgbBqiyRkdr9ZGZGoc3GZdxOqXDYDuTKcdQ0ic7F_Ejn5afhz'),
+          placeholder: AssetImage('assets/original.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
