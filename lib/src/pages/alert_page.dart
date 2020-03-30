d6ffca9ab1be801ba2f07521a9237f3e7c72388c
JuhJSimoes <juianajsimoes@gmail.com>
2020-03-30 17:56:06 -0300

    Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta!'),
+          color: Colors.blue,
+          onPressed: () => _mostrarAlerta(context),
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        )
+      ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.arrow_back),
           onPressed: () {
@@ -14,4 +23,39 @@ class AlertPage extends StatelessWidget {
           }),
     );
   }
+
+  void _mostrarAlerta(BuildContext context){
+
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Esse é o conteudo do alerta'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+
 }
