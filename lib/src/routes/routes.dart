0c52315f6f4e984582525639707a03d374e19a52
JuhJSimoes <juianajsimoes@gmail.com>
2020-03-31 00:55:31 -0300

    Listas, ListController

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:projetocomponentesflutter/src/pages/avatar_page.dart';
 import 'package:projetocomponentesflutter/src/pages/card_page.dart';
 import 'package:projetocomponentesflutter/src/pages/home_page.dart';
 import 'package:projetocomponentesflutter/src/pages/input_page.dart';
+import 'package:projetocomponentesflutter/src/pages/listview_page.dart';
 import 'package:projetocomponentesflutter/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -16,5 +17,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'animationContainer': (BuildContext context) => AnimationContainerPage(),
     'inputs'            : (BuildContext context) => InputPage(),
     'slider'            : (BuildContext context)  => SliderPage(),
+    'list'              : (BuildContext context) => ListaPage(),
   };
 }
